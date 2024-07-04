e198489a465cc8f857a5643b0250186ccfa3e7e7
Daniel Luoma <luomadaniel95@gmail.com>
2024-07-04 16:29:48 +0300

    Add working ffi. C reaches the rust function

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+extern const char *get_string(void);
+
+int main() {
+    char* string = get_string();
+    printf("%s", string);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+pub fn get_string() -> *const u8 {
+    b"Hello, C-World\0".as_ptr()
 }
