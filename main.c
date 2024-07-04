271c3ccc3659b7cd434e0faa5d1719eaea23502f
Daniel Luoma <luomadaniel95@gmail.com>
2024-07-04 16:35:00 +0300

    Add passing arguments from C to rust

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-extern const char *get_string(void);
+extern const char *get_string(char);
 
 int main() {
-    char* string = get_string();
+    char* string = get_string(9);
     printf("%s", string);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #[no_mangle]
-pub fn get_string() -> *const u8 {
+pub fn get_string(end: u8) -> *const u8 {
+    print!("You passed in an argument {}\n", end);
     b"Hello, C-World\n\0".as_ptr()
 }
